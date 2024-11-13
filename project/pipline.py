5e6d4ac17bac68d808cb817e75570df6c363d1b0
behrooz bozorg chami <behroozbc@outlook.com>
2024-11-13 20:12:30 +0100

    fix the issue #1

--- project/pipline.py
+++ project/pipline.py
@@ -1,10 +1,14 @@
 import kagglehub
 import os
 import shutil
+from pirateweather import forecast
 
 # Download latest version
-path = kagglehub.dataset_download("zernach/2018-airplane-flights",)
+path = kagglehub.dataset_download("zernach/2018-airplane-flights")
 for file in os.listdir(path):
     filename=os.path.basename(file)
     shutil.copy(os.path.join(path,file),f"data/{filename}")
-print("Path to dataset files:", path)
+path = kagglehub.dataset_download("behroozbc/average-day-weather-for-2018")
+for file in os.listdir(path):
+    filename=os.path.basename(file)
+    shutil.copy(os.path.join(path,file),f"data/{filename}")
