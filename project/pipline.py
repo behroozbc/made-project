8f88f67e619670c2fedb59311dad154247f1404d
behrooz bozorg chami <behroozbc@outlook.com>
2024-11-23 20:37:42 +0100

    fix pipeline

--- project/pipline.py
+++ project/pipline.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from pirateweather import forecast
 import pandas as pd
+QuarterRange=[{'start':'2018-01-01','end':'2018-03-31','index':1},{'start':'2018-04-01','end':'2018-06-31','index':2},{'start':'2018-07-01','end':'2018-09-31','index':3},{'start':'2018-10-01','end':'2018-12-31','index':4}]
 # Download latest version
 path = kagglehub.dataset_download("zernach/2018-airplane-flights")
 for file in os.listdir(path):
@@ -14,5 +15,15 @@ for file in os.listdir(path):
     shutil.copy(os.path.join(path,file),f"data/{filename}")
 df= pd.read_csv('data\Avg-day-2018.csv')
 df=df[['DATE','DailyAverageDewPointTemperature']]
-df.to_csv('data\Avg-day-2018.csv')
+meanWeathers=[]
+for quarter in QuarterRange:
+    quraterRange=df[(df['DATE'] > quarter['start']) & (df['DATE'] < quarter['end'])]
+    meanWeathers.append({'AvgTemperature':quraterRange['DailyAverageDewPointTemperature'].astype(float).mean(),'quarter':quarter['index']})
+pd.DataFrame(meanWeathers).to_csv('data\Avg-day-2018.csv')
 df=pd.read_csv('data\Cleaned_2018_Flights.csv')
+resultdf=[]
+for quarter in QuarterRange:
+    quraterRange=df[df['Quarter']==quarter['index']]
+    resultdf.append({'PricePerMiles':((quraterRange['PricePerTicket'].astype(float))/quraterRange['Miles'].astype(float)).mean(),'quarter':quarter['index']})
+
+pd.DataFrame(resultdf).to_csv('data\Cleaned_2018_Flights.csv')
