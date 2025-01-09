4aa6b99fa9166baeff8f4c5b5bc06d3574164212
behrooz bozorg chami <behroozbc@outlook.com>
2025-01-09 19:07:55 +0100

    fix

--- project/pipline.py
+++ project/pipline.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 QuarterRange=[{'start':'2018-01-01','end':'2018-03-31','index':1},{'start':'2018-04-01','end':'2018-06-31','index':2},{'start':'2018-07-01','end':'2018-09-31','index':3},{'start':'2018-10-01','end':'2018-12-31','index':4}]
 # Download latest version
-def download_dataset(name:str):
+def download_dataset(name:str)->str:
     path=kagglehub.dataset_download(name)
     return os.path.join(path,os.listdir(path)[0])
-def copy_to_data(fullPath2File:str):
+def copy_to_data(fullPath2File:str) -> str:
     savePath=f"{os.getcwd()}/main/data/{os.path.basename(fullPath2File)}"
     shutil.copy(fullPath2File,savePath)
     return savePath
@@ -24,8 +24,13 @@ def transform(flightsdf:pd.DataFrame,weatherdf:pd.DataFrame):
         flights_quarter_range=flightsdf[flightsdf['Quarter']==quarter['index']]
         resultdf.append({'AvgTemperature':quraterRange['DailyAverageDewPointTemperature'].astype(float).mean(),'PricePerMiles':((flights_quarter_range['PricePerTicket'].astype(float))/flights_quarter_range['Miles'].astype(float)).mean(),'quarter':quarter['index']})        
     return pd.DataFrame(resultdf)
-
+def clean(paths:[str]):
+    for path in paths:
+        os.remove(path)
 if __name__=='__main__':
-    resultdf=transform(read_file(copy_to_data(download_dataset('zernach/2018-airplane-flights'))),read_file(copy_to_data(download_dataset('behroozbc/average-day-weather-for-2018'))))
+    flightpth=copy_to_data(download_dataset('zernach/2018-airplane-flights'))
+    weatherpth=copy_to_data(download_dataset('behroozbc/average-day-weather-for-2018'))
+    resultdf=transform(read_file(flightpth),read_file(weatherpth))
+    clean([flightpth,weatherpth])
     save_processed_data(resultdf,'data\\final.csv')
 
